58fb0662f3cb53f7a750b8483e0d8ec93c49a000
Dominik Gadesmann <dominik.gadesmann@gmail.com>
2020-09-11 14:42:49 +0200

    fixed editor formatting

--- main.py
+++ main.py
@@ -6,26 +6,26 @@ game = Game()
 
 y = 0
 for i, line in enumerate(file):
-    if i > 0 and i < 8:
-        for x, icon in enumerate(line[:-1]):
-            if icon == '#':
-                game.gameMap.walls.append([x, y])
-            if icon == '@':
-                game.startState.playerCoords = [x, y]
-            if icon == '$':
-                game.startState.boxes.append([x, y])
-            if icon == '*':
-                game.startState.boxes.append([x, y])
-                game.gameMap.goals.append([x, y])
-            if icon == '.':
-                game.gameMap.goals.append([x, y])
-        y += 1
+  if i > 0 and i < 8:
+    for x, icon in enumerate(line[:-1]):
+      if icon == '#':
+        game.gameMap.walls.append([x, y])
+      if icon == '@':
+        game.startState.playerCoords = [x, y]
+      if icon == '$':
+        game.startState.boxes.append([x, y])
+      if icon == '*':
+        game.startState.boxes.append([x, y])
+        game.gameMap.goals.append([x, y])
+      if icon == '.':
+        game.gameMap.goals.append([x, y])
+  y += 1
 
 print(game.startState.playerCoords)
 
 
 def wrapperFunction(game):
-    gameStates = [game.startState]
+  gameStates = [game.startState]
 
 
 def checkGameFinished():
